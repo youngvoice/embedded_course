1ad8b0ac36d71563f278163d6a4779bd9ce30f5e
xjk <xjk@gmail.com>
2018-11-10 16:25:52 +0800

    add uart receive and transmit

--- yangjunliang/Lab09-1_RS232/step2step.c
+++ yangjunliang/Lab09-1_RS232/step2step.c
@@ -3,9 +3,15 @@
 #include <stdio.h>
 #include "dr_tft.h"
 
+unsigned char flag0=0,flag1=0;
+unsigned char send_data[]={'0','\0'};
+unsigned char recv_data[]={'0','\0'};
+
 void GPIO_init(void)
 void TimerA_Init(void);		//定时器TA初始化函数
 void initClock(void);
+void UART_RS232_Init(void);	//RS232接口初始化
+
 
 void initialization(void);
 
@@ -16,31 +22,33 @@ void main(void)
 
 	_DINT();
 	initialization();
-	etft_AreaSet(0,0,319,239,0);
+	etft_AreaSet(0,0,319,239,0);//TFT清屏
+	etft_DisplayString("Send Data: ",80,100,65535,0);	//TFT屏显示发送数据的标识
+	etft_DisplayString("Recv Data: ",80,140,65535,0);	//TFT屏显示接收数据的标识
+
 
 	_EINT();
 	
 
 	while(1)
 	{
-		etft_AreaSet(0,0,39,239,0);
-		etft_AreaSet(40,0,79,239,31);
-		etft_AreaSet(80,0,119,239,2016);
-		etft_AreaSet(120,0,159,239,63488);
-		etft_AreaSet(160,0,199,239,2047);
-		etft_AreaSet(200,0,239,239,63519);
-		etft_AreaSet(240,0,279,239,65504);
-		etft_AreaSet(280,0,319,239,65535);
-		__delay_cycles(MCLK_FREQ*3);
-		etft_AreaSet(0,0,319,239,0);
-		__delay_cycles(MCLK_FREQ);
-		etft_DisplayString("TI MSP430F6638 EVM",100,80,65535,0);
-		etft_DisplayString("TI UNIVERSITY PROGRAM",0,150,63488,0);
-		etft_DisplayString("- TSINGHUA UNIVERSITY",100,180,65504,0);
-		__delay_cycles(MCLK_FREQ*3);
+		if(flag0)			//flag0在定时器计时到达1s时被赋值为1
+		{
+			flag0=0;
+			UCA1TXBUF=send_data[0];		//写一个字符到发送缓存发送数据
+			etft_DisplayString(send_data,170,100,65535,0);	//TFT屏上显示发送的字符
+			send_data[0]++;				//字符加1
+			if(send_data[0]>'9')		//字符超过'9'则重新置'0'
+				send_data[0]='0';
+		}
 
-		// background
+		if(flag1)			//flag1在接收中断中被赋值为1
+		{
+			flag1=0;
+			etft_DisplayString(recv_data,170,140,65535,0);	//TFT屏幕上显示接收到的字符
+		}
 	}
+
 }
 
 void initialization(void)
@@ -49,6 +57,7 @@ void initialization(void)
 	TimerA_Init();
 	initClock();
   	initTFT();
+  	UART_RS232_Init();
 }
 void GPIO_init(void)
 {
@@ -91,12 +100,55 @@ void initClock(void)
 }
 
 
+void UART_RS232_Init(void)	//RS232接口初始化函数
+{
+	/*通过对P3.4。P3.5，P4.4，P4.5的配置实现通道选择
+	 	 使USCI切换到UART模式*/
+	P3DIR|=(1<<4)|(1<<5);
+	P4DIR|=(1<<4)|(1<<5);
+	P4OUT|=(1<<4);
+	P4OUT&=~(1<<5);
+	P3OUT|=(1<<5);
+	P3OUT&=~(1<<4);
+	P8SEL|=0x0c;	//模块功能接口设置，即P8.2与P8.3作为USCI的接收口与发射口
+	UCA1CTL1|=UCSWRST;	//复位USCI
+	UCA1CTL1|=UCSSEL_1;	//设置辅助时钟，用于发生特定波特率
+	UCA1BR0=0x03;		//设置波特率
+	UCA1BR1=0x00;
+	UCA1MCTL=UCBRS_3+UCBRF_0;
+	UCA1CTL1&=~UCSWRST;	//结束复位
+	UCA1IE|=UCRXIE;		//使能接收中断
+}
+
+
 //foreground
 //TimerA
-#pragma vector = TIMER0_A0_VECTOR
+#pragma vector = TIMER0_A0_VECTOR	//定时器TA中断服务函数
 __interrupt void Timer_A (void)
 {
-	P1OUT ^= BIT5;//形成鸣叫效果
-	P4OUT ^= BIT5;//形成闪灯效果
+	static unsigned char i=0;
+	i++;
+	if(i>=20)				//记满二十次为1s
+	{
+		i=0;
+		flag0=1;			//改变标识数据的值
+	}
 }
+
 //UART
+#pragma vector=USCI_A1_VECTOR	//USCI中断服务函数
+__interrupt void USCI_A1_ISR(void)
+{
+	switch(__even_in_range(UCA1IV,4))
+	{
+	case 0:break;			//无中断
+	case 2:					//接收中断处理
+		while(!(UCA1IFG&UCTXIFG));	//等待完成接收
+		recv_data[0]=UCA1RXBUF;		//数据读出
+		flag1=1;					//置标识数据的值
+		break;
+	case 4:break;			//发送中断不处理
+	default:break;			//其他情况无操作
+
+	}
+}
